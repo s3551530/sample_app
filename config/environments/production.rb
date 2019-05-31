2e3de61377fc3a11b1be5ba36120694ba4a1184d
EC2 Default User <ec2-user@ip-172-31-82-92.ec2.internal>
2019-05-31 02:33:08 +0000

    Initial commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'mighty-fjord-11274.herokuapp.com'
+  host = 'mysterious-river-33039.herokuapp.com/'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
